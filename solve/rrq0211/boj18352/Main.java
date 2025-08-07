4575c1ce4edd6bd4a93278ef3fb25044098b88e9
권기범 <xtcc02332@naver.com>
2025-08-07 16:31:54 +0900

    Solve: 0807 E BOJ 5567

--- solve/rrq0211/boj18352/Main.java
+++ solve/rrq0211/boj18352/Main.java
@@ -17,7 +17,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int n = Integer.parseInt(st.nextToken());
