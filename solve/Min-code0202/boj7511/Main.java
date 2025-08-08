e1b356526f86c88b906558d84b4ab13649629b5c
정재민 <jurumin0015@gmail.com>
2025-08-08 17:23:32 +0900

    Solve: 0804 N BOJ 7511

---
+++ solve/Min-code0202/boj7511/Main.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int[] parent;
+
+    boolean isSame(int a, int b){
+        return find(a) == find(b);
+    }
+
+    void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a != b){
+            parent[b] = a;
+        } 
+    }
+
+    int find(int a){
+        if(a == parent[a]) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    public void solution() throws IOException {
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            wr.write("Scenario " + t + ":\n");
+
+            int n = Integer.parseInt(br.readLine());
+            int k = Integer.parseInt(br.readLine());
+
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+
+            for (int i = 0; i < k; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                union(a, b);
+            }
+
+            int m = Integer.parseInt(br.readLine());
+            for (int i = 0; i < m; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                if (isSame(a, b)) {
+                    wr.write("1\n");
+                } else {
+                    wr.write("0\n");
+                }
+            }
+
+            wr.write("\n");
+        }
+
+        wr.flush();
+        wr.close();
+        br.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+}
