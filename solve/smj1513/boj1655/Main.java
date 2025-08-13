cab3b5057a97b1b49660c8bb7f041552ab282e19
성민제 <minj21@naver.com>
2025-08-13 11:05:05 +0900

    Solve: 0813 E BOJ 10974

--- solve/smj1513/boj1655/Main.java
+++ solve/smj1513/boj1655/Main.java
@@ -1,4 +1,34 @@
 package smj1513.boj1655;
 
+import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> large = new PriorityQueue<>();
+        PriorityQueue<Integer> small = new PriorityQueue<>(Comparator.reverseOrder());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < N ; i++){
+            int n = Integer.parseInt(br.readLine());
+            small.add(n);
+            if(!large.isEmpty() && !small.isEmpty() && small.peek() > large.peek()){
+                large.add(small.poll());
+            }
+            if(small.size() > large.size()+1){
+                large.add(small.poll());
+            }
+            if(small.size() < large.size()){
+                small.add(large.poll());
+            }
+
+            sb.append(small.peek()).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
