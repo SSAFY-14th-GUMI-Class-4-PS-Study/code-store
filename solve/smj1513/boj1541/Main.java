94c1f760717c8d866d1afbf3a72b4f3bf2a22c94
성민제 <minj21@naver.com>
2025-08-14 13:59:33 +0900

    Solve: 0814 E BOJ 1541

--- solve/smj1513/boj1541/Main.java
+++ solve/smj1513/boj1541/Main.java
@@ -5,8 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.stream.IntStream;
 
+
+/*
+* 그리디 하게 생각하면 풀리는 문제
+* 괄호가 쳐지지 않은 수식에서 가장 작은 값을 만드는 경우는 빼는 값이 가장 크면 됨.
+* 55-50+40 -> 55-(50+40) 처럼 - 뒤의 값을 무조건 먼저 연산 해서 +로 가장 크게 만들면 됨
+* split에서 +는 안되니 \\+로 입력
+* */
 public class Main {
     static int[] sums;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
