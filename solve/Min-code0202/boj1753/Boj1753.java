39760f45c7bb01ee296b099784fea693f6b7952c
Min-code0202 <jjm001115@gmail.com>
2025-08-21 17:28:06 +0900

    Solve: 0819 N BOJ 1753

--- solve/Min-code0202/boj1753/Boj1753.java
+++ solve/Min-code0202/boj1753/Boj1753.java
@@ -1,5 +1,11 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 class Node implements Comparable<Node> {
     int v, w;
