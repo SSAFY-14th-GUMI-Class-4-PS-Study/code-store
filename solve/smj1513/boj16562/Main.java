13561b4ef5782e9444b404c0d48b527bd5c7501e
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2025-08-31 18:30:05 +0900

    Solve: 0825 N BOJ 16562

--- solve/smj1513/boj16562/Main.java
+++ solve/smj1513/boj16562/Main.java
@@ -41,15 +41,19 @@ public class Main {
 			System.out.println(resultCost);
 		}
 	}
-
+	// 두 학생(그룹)을 하나로 합치는 union 연산
 	public static boolean union(int a, int b) {
+		// 각 학생이 속한 그룹의 대표(루트)를 찾음
 		int fa = find(a);
 		int fb = find(b);
+
+		// 두 학생이 이미 같은 그룹이 아니라면 합침
 		if (fa != fb) {
+			// 두 그룹을 합칠 때, 친구비가 더 저렴한 학생을 새로운 그룹의 대표로 만듦
 			if (friendCosts[fa] < friendCosts[fb]) {
-				parents[fb] = fa;
+				parents[fb] = fa; // b그룹이 a그룹으로 흡수됨 (대표는 fa)
 			} else {
-				parents[fa] = fb;
+				parents[fa] = fb; // a그룹이 b그룹으로 흡수됨 (대표는 fb)
 			}
 			return true;
 		} else {
